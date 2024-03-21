9c86ad4077c0f4fb3fb592366cadf3de1c435062
abhinawagoo <abhinawago@gmail.com>
2024-03-21 07:32:10 +0530

    updating cors

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ dotenv.config({ path: ".env" });
 import taskRoutes from "./routes/task.js";
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import connectToDatabase from "./config/dbConfig.js";
 //express app
 const app = express();
-
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://node-task-backend-rho.vercel.app"],
+  credentials: true,
+};
 //middleware
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 app.use("/api", connectToDatabase, taskRoutes);
 
 // app.js
